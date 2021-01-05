9664b634dd457eaf57f096d28ed64f830d72b9ba
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-05 19:30:02 +0700

    Create AlarmReceiver.kt

---
+++ app/src/main/java/io/erikrios/github/myalarmmanager/AlarmReceiver.kt
@@ -0,0 +1,13 @@
+package io.erikrios.github.myalarmmanager
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("AlarmReceiver.onReceive() is not implemented")
+    }
+}
