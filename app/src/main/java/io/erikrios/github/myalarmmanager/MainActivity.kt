f3724e059271273721ae1f099fed7fa144fb766d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-05 20:24:19 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/myalarmmanager/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myalarmmanager/MainActivity.kt
@@ -1,11 +1,19 @@
 package io.erikrios.github.myalarmmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.myalarmmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    private lateinit var alarmReceiver: AlarmReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
     }
 }
