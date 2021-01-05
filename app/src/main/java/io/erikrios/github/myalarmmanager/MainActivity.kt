21bdb1a13e6ecf059367adcec55bda4cfd1957e8
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-05 20:27:25 +0700

    Add click handler for the button

--- app/src/main/java/io/erikrios/github/myalarmmanager/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myalarmmanager/MainActivity.kt
@@ -1,10 +1,11 @@
 package io.erikrios.github.myalarmmanager
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import io.erikrios.github.myalarmmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var binding: ActivityMainBinding? = null
     private lateinit var alarmReceiver: AlarmReceiver
@@ -15,5 +16,30 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        binding?.btnOnceDate?.setOnClickListener(this)
+        binding?.btnOnceTime?.setOnClickListener(this)
+        binding?.btnSetOnceAlarm?.setOnClickListener(this)
+
+        alarmReceiver = AlarmReceiver()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            binding?.btnOnceDate?.id -> {
+
+            }
+            binding?.btnOnceTime?.id -> {
+
+            }
+            binding?.btnSetOnceAlarm?.id -> {
+
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
