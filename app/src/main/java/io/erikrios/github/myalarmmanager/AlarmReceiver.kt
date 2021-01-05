66e1f161cd61cb1bf83a83f5f506b036e0971821
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-01-05 21:24:19 +0700

    Refactor the code

--- app/src/main/java/io/erikrios/github/myalarmmanager/AlarmReceiver.kt
+++ app/src/main/java/io/erikrios/github/myalarmmanager/AlarmReceiver.kt
@@ -80,7 +80,6 @@ class AlarmReceiver : BroadcastReceiver() {
         alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pendingIntent)
 
         Toast.makeText(context, "One time alarm set up", Toast.LENGTH_LONG).show()
-
     }
 
     private fun showAlarmNotification(
